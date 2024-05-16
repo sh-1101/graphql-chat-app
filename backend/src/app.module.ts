bb22b2a9b492077712fb6dd1bc16346129277f49
Hiroki Sugiyama <hiroki.sugiyama.dev@gmail.com>
2024-05-17 08:09:40 +0900

    docs: 処理コメント記述

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -12,19 +12,26 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
   imports: [
     AuthModule,
     UserModule,
+    //非同期（forRootAsync）でGraphQLモジュールを設定するための方法
     GraphQLModule.forRootAsync({
       imports: [ConfigModule, AppModule],
-      inject: [ConfigService],
-      driver: ApolloDriver,
+      inject: [ConfigService], //ConfigServiceを注入する。これにより設定値をuseFactory関数内で利用できる
+      driver: ApolloDriver, //Apolloのドライバーを使用してGraphQLサーバーを動かす
       useFactory: async (configService: ConfigService) => {
+        //useFactory: 非同期かつ動的に設定オブジェクトを生成するための関数
         return {
+          //GraphQL PlayGroundを有効にする。ブラウザ上でGraphQLクエリを試すことが可能
           playground: true,
+          //GraphQLスキーマを自動生成してファイルに保存する。process.cwd()は現在の作業ディレクトリを指す
           autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+          //スキーマをソートする。これにより生成されるスキーマファイルが一貫した順序で出力される
           sortSchema: true,
         };
       },
     }),
     ConfigModule.forRoot({
+      //この設定によりConfigModuleがアプリケーション全体でグローバルに利用可能になるため、
+      //他のモジュールでConfigServiceを個別にインポートする必要がなくなる
       isGlobal: true,
     }),
   ],
